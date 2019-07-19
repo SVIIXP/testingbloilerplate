3af7ee1b7af1694e6b9dc2a997385f48914f88b9
Saulo Pereira <saulo.pereira@gmail.com>
2019-07-19 18:39:32 -0300

    linting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
@@ -5,5 +6,6 @@ import './index.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  // eslint-disable-next-line no-undef
+  document.getElementById('root'),
 );
